2e7e2f5f1e328460d65349be38b651aded412d9e
Teja Kumar <batcuteja143@gmail.com>
2026-05-30 01:21:10 +0530

    "Successfully completed Assingment Operator"

---
+++ Day_6_Assignment_Operator.py
@@ -0,0 +1,129 @@
+'''print("Addition Assignment Operator")
+num_1 = 20
+num_1 += 10
+print(f"Num1: {num_1}")
+
+num_2 = 7
+num_2 += 3
+print(f"Num2: {num_2}")
+num_2 += 5
+print(f"Num2: {num_2}")
+
+name = "Teja"
+name += " Kumar"
+print(f"Name: {name}")
+
+#Taking the value from the user
+
+number = int(input("Enter a random number: "))
+number += int(input("Enter another number: "))
+print(f"Number: {number}")
+
+#Asking the user to enter a name
+random_name = input("Enter a name: ")
+random_name += input("Enter another name:  ")
+print(f"Randon Name:{random_name}")
+
+#Subtraction
+print("Subtraction Assignment Operator")
+num_3 = 25
+num_3 -= 12
+print(f"Num3: {num_3}")
+
+num_4 = 35
+num_4 -= 8
+print(f"Num4: {num_4}")
+num_4 -= 15
+print(f"Num4: {num_4}")
+
+number = int(input("Enter a random number: "))
+number -= int(input("Enter another number: "))
+print(f"Number: {number}")
+
+#Multiplication
+print("Multiplication Assignment Operator")
+number_5 = 5
+number_5 *= 6
+print(f"Number5: {number_5}")
+
+number_6 = 10
+number_6 *= 12
+print(f"Number6: {number_6}")
+number_6 *= 10
+print(f"Number6: {number_6}")
+
+number = int(input("Enter a random number: "))
+number *= int(input("Enter another number: "))
+print(f"Number: {number}")
+
+name = input("Enter a random word: ")
+name *= int(input("Enter a random number: "))
+print(f"Name: {name}")
+
+print("Division Assignment Operator")
+
+num_7 = 18
+num_7 /= 4
+print(f"Num7: {num_7}")
+print(f"Num7: {int(num_7)}")
+
+num_8 = int(input("Enter a random number: "))
+num_8 /= int(input("Enter a random number: "))
+print(f"Num8: {num_8}")
+print(f"Num8: {int(num_8)}")
+
+num_9 = 145
+num_9 /= 6
+print(f"Num9: {num_9}")
+print(f"Num9: {int(num_9)}")
+num_9 /= 4
+print(f"Num9: {num_9}")
+print(f"Num9: {int(num_9)}")
+
+
+print("Floor Division Assingment Operator")
+
+num_10 = 19
+num_10 //= 3
+print(f"Num10: {num_10}")
+
+num_11 = int(input("Enter a random number: "))
+num_11 //= int(input("Enter another random number: "))
+print(f"Num11: {num_11}")
+
+num_12 = 356
+num_12 //= 23
+print(f"Num12: {num_12}")
+num_12 //= 12
+print(f"Num12: {num_12}")'''
+
+print("Modulus Assingment Operator")
+num_13 = 16
+num_13 %= 3
+print(f"Num13: {num_13}")
+
+num_14 = 289
+num_14 %= 7
+print(f"Num14: {num_14}")
+num_14 %= 3
+print(f"Num14: {num_14}")
+
+num_15 = int(input("Enter a random number: "))
+num_15 %= int(input("Enter another random number: "))
+print(f"Num15: {num_15}")
+
+print("Power Assingment Operator")
+
+num_16 = 3
+num_16 **= 2
+print(f"Num16: {num_16}")
+
+num_17 = 4
+num_17 **= 2
+print(f"Num17: {num_17}")
+num_17 **= 3
+print(f"Num17: {num_17}")
+
+num_18 = int(input("Enter a random number: "))
+num_18 **= int(input("Enter a random number between 1 to 5: "))
+print(f"NUm18: {num_18}")
